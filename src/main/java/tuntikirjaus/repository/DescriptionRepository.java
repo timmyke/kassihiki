85136c5c3594ac9bde7e6caa0a8d069695096e11
Timi <timmyke@server>
2014-09-24 21:22:22 +0300

    Projekti luotu uusiksi

--- src/main/java/tuntikirjaus/repository/DescriptionRepository.java
+++
@@ -1,8 +0,0 @@
-package tuntikirjaus.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tuntikirjaus.domain.Description;
-
-public interface DescriptionRepository extends JpaRepository<Description, Long> {
-
-}
